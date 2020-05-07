1354cf30359e034a4ec788af5ac947b75b65f0d2
Aaron Verstappen <aaronverstappen@gmail.com>
2020-05-07 20:56:57 +0200

    Remove website files

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var mailtoLinks = document.querySelectorAll("[href*='mailto:']");
-	if (mailtoLinks) {
-		for (var i = 0; i < mailtoLinks.length; i++) {
-			mailtoLinks[i].setAttribute("target","_blank");
-		}
-	}
-})();
